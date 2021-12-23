866ea19564d511c5ff5e5607ec3f1847014aef29
vevdokimenko <evdokimenko@hotmail.com>
2021-12-23 20:19:44 +0200

    Practice

---
+++ src/main/java/com/itvdn/Main.java
@@ -0,0 +1,31 @@
+package com.itvdn;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static SessionFactory sessionFactory;
+
+    public static void main(String[] args) {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        Person person = new Person();
+        person.setName("Alex");
+        person.setAge(30);
+        person.setGender("Male");
+
+        commitPerson(person);
+    }
+
+    private static void commitPerson(Person person) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(person);
+
+        transaction.commit();
+        session.close();
+    }
+}
