4c840fd9b8246fa25e05e7f824c04506620360a6
vevdokimenko <evdokimenko@hotmail.com>
2021-12-23 20:27:49 +0200

    Practice

--- src/main/java/com/itvdn/Main.java
+++ src/main/java/com/itvdn/Main.java
@@ -9,17 +9,16 @@ public class Main {
     public static SessionFactory sessionFactory;
 
     public static void main(String[] args) {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
-
         Person person = new Person();
         person.setName("Ivan");
         person.setAge(28);
         person.setGender("Male");
 
-        commitPerson(person);
+        commitEntity(person);
     }
 
-    private static void commitPerson(Person person) {
+    private static void commitEntity(Person person) {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
